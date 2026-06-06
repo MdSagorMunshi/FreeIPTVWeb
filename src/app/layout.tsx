e34f21ed1b3dafc494c295bbe389a303998f5863
Ryan Shelby <ryn@disr.it>
2026-06-06 11:07:33 +0600

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { AppGate } from "@/components/AppGate";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             {children}
           </AppGate>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
